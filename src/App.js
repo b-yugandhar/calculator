93b895f7adefdac88887d20610d558ab53665bac
b-yugandhar <yugandharmb0@gmail.com>
2022-05-05 19:26:17 +0530

    calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+const App =()=>{
+    const [data,setData] =useState("hi")
+    const changehandler = e =>{
+        setData(data.concat(e.target.name))
+    }
+    const clearhandler =e =>{
+      setData("")
+    }
+    const backhandler =e=>{
+      setData(data.slice(0,-1))
+    }
+    const resulthandler=e=>{
+      setData(eval(data).toString());
+    }
+    return(
+      <div  className='container'>
+          <h1>CALCULATOR</h1>
+        <form >
+          <input id='inputbar' type="text" value={data}/>
+        </form>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div className="keyboard">
+          <button onClick={clearhandler} id='clear'>clear</button>
+          <button id='color' onClick={backhandler}>C</button>
+          <button id='color' onClick={changehandler} name='/' >%</button>
+
+          <button onClick={changehandler} name="1">1</button>
+          <button onClick={changehandler} name='2'>2</button>
+          <button onClick={changehandler} name='3'>3</button>
+          <button onClick={changehandler} id='color' name='+'>+</button>
+          <button onClick={changehandler} name='4'>4</button>
+          <button onClick={changehandler} name='5'>5</button>
+          <button onClick={changehandler} name='6'>6</button>
+          <button onClick={changehandler} id='color' name='-'>-</button>
+          <button onClick={changehandler} name='7'>7</button>
+          <button onClick={changehandler} name='8'>8</button>
+          <button onclick={changehandler} name='9'>9</button>
+          <button onClick={changehandler} id='color' name='*'>*</button>
+          <button onClick={changehandler} id='color' name='.'>.</button>
+          <button onClick={changehandler} name='0' >0</button>
+          <button onClick={resulthandler}  id='result'>=</button>
+          
+         
+        </div>
+      </div>
+    )
 }
 
-export default App;
+export default App
